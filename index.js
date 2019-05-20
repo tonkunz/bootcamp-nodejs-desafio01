82169f40c938eb4fd74e584bc9a6ed0fdd234fc7
Everton Kunz <ehokeverton@gmail.com>
2019-05-19 23:44:51 -0300

    Adicionado rotas major e minor.

--- index.js
+++ index.js
@@ -9,10 +9,32 @@ nunjucks.configure('views', {
   watch: true
 })
 
+app.use(express.urlencoded({ extended: false }))
 app.set('view engine', 'njk')
 
 app.get('/', (req, res) => {
   res.render('home')
+})
+
+app.post('/check', (req, res) => {
+  const { age } = req.body
+
+  if (req.body.age >= 18) {
+    res.redirect(`/major?age=${age}`)
+  } else {
+    res.redirect(`/minor?age=${age}`)
+  }
+})
+
+app.get('/major', (req, res) => {
+  const { age } = req.query
+
+  return res.render('major', { age })
+})
+
+app.get('/minor', (req, res) => {
+  const { age } = req.query
+  return res.render('minor', { age })
 })
 
 app.listen(3001, () => {
